8656679eacdc85fde948761fb330d5e846f00908
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-02-08 23:31:08 -0500

    initial commit

--- tests/sae/test_eb_area_model.py
+++ tests/sae/test_eb_area_model.py
@@ -7,4 +7,10 @@ from samplics.sae.eb_area_model import AreaModel
 
 milk = pd.read_csv("./tests/sae/milk.csv")
 
-print(milk)
+area = milk["SmallArea"]
+yhat = milk["yi"]
+X = pd.get_dummies(milk["MajorArea"])
+sigma2_e = milk["SD"] ** 2
+
+
+fh_model = AreaModel("FH").fit(area=area, yhat=yhat, X=X, sigma2_e=sigma2_e)
