bddd05296e1f5debf87d2fa95730a60aa45d7f38
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-04-02 21:11:18 -0400

    initial commit

--- src/samplics/__init__.py
+++ src/samplics/__init__.py
@@ -1,6 +1,7 @@
 from samplics.estimation import *
 from samplics.sampling import *
 from samplics.weighting import *
+from samplics.sae import *
 
 from samplics.utils.formats import array_to_dict
 from samplics.utils.basic_functions import BoxCox
