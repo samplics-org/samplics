5648ba9a167ddfd2a074436a8ba14c631ae1635f
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-05-07 21:40:33 -0400

    initial commit

---
+++ tests/sae/simulate_lmm_population.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+
+import statsmodels.api as sm
+
+
+np.random.seed(12345)
+
+# model parameters
+scale = 1
+sigma2e = 1 ** 2
+sigma2u = 0.25 ** 2
+# print(sigma2u / (sigma2u + sigma2e / 50))
+
+# Population sizes
+N = 6000
+nb_areas = 20
+
+
+error = np.random.normal(loc=0, scale=(scale ** 2) * (sigma2e ** 0.5), size=N)
+area = np.sort(np.random.choice(range(1, nb_areas + 1), N))
+areas, Nd = np.unique(area, return_counts=True)
+
+random_effects = np.random.normal(loc=0, scale=sigma2u ** (1 / 2), size=nb_areas)
+total_error = np.repeat(random_effects, Nd) + error
+
+# Auxiliary information
+p1 = 0.3 + areas / np.max(areas)
+p1 = p1 / sum(p1)
+p1 = p1 / (1.2 * max(p1))
+
+X1 = np.array([])
+for k, s in enumerate(Nd):
+    Xk = np.random.binomial(1, p=p1[k], size=s)
+    X1 = np.append(X1, Xk)
+X2 = 0.01 + np.random.beta(0.5, 1, N)
+X3 = np.random.binomial(1, p=0.6, size=N)
+
+X = np.column_stack((np.ones(N), X1, X2, X3))
+beta = np.array([5, 3, -3, 3])
+Y = np.exp(np.matmul(X, beta) + total_error)
+
+average_nd = 30
+sample = np.random.binomial(1, average_nd * nb_areas / N, N)
+
+# simulated_lmm_population = pd.DataFrame(
+#     np.column_stack((Y, area, X[:, 1:4], sample)), columns=["Y", "area", "X1", "X2", "X3", "sample"],
+# ).astype({"area": "int16", "X1": "int8", "X3": "int8", "sample": "int8"})
+
+# simulated_lmm_population.to_csv("./tests/sae/simulated_lmm_population_seed12345.csv", index=False)
--- tests/sae/test_eb_unit_model.py
+++ tests/sae/test_eb_unit_model.py
@@ -1,22 +1,65 @@
 import numpy as np
 import pandas as pd
 
-from samplics.sae.eb_unit_model import UnitModelBHF
+from samplics.sae.eb_unit_model import EbUnitLevel
 
-from samplics.utils.basic_functions import BoxCox
+lmm_pop = pd.read_csv("./tests/sae/simulated_lmm_population_seed12345.csv")
 
-income = pd.read_csv("./tests/sae/income.csv")
+area = lmm_pop["area"]
+Y = lmm_pop["Y"]
+X = lmm_pop[["X1", "X2", "X3"]]
+sample = lmm_pop["sample"]
+
+# Sample data
+area_s = area[sample == 1]
+y_s = Y[sample == 1]
+X_s = X[sample == 1]
 
 
-y = income["income"]
+# Out of sample data
+area_r = area[sample != 1]
+X_r = X[sample != 1]
 
-# y = np.random.normal(size=100000)
 
-print(BoxCox().get_skewness(y))
-print(BoxCox().get_kurtosis(y))
+# Molina-Rao Method
+def pov_gap(y, pov_line=500):
+    return np.mean((y < pov_line) * (pov_line - y) / pov_line)
 
-print(BoxCox().transform(y, 0))
 
-# BoxCox().plot_skewness(y)
+# eb_bhf_reml = EbUnitLevel(method="REML", boxcox=0, constant=10, indicator=pov_gap)
+# eb_bhf_reml.fit(y_s, X_s, area_s, intercept=True)
 
-BoxCox().plot_kurtosis(y, -0.1, 2)
+# print(f"EB fixed effects: {eb_bhf_reml.fixed_effects}")
+# print(f"EB sigma u: {eb_bhf_reml.re_std}")
+# print(f"EB sigma e: {eb_bhf_reml.error_std}\n")
+
+# eb_bhf_reml.predict(200, pov_gap, X_r, area_r)
+# print(f"EB area parameter:\n {eb_bhf_reml.area_est}\n")
+
+# mse = eb_bhf_reml.bootstrap_mse(50, pov_gap, X_r, area_r,)
+# print(f"EB area mse:\n {mse}\n")
+
+
+"""REML Method"""
+
+
+eb_bhf_reml = EbUnitLevel(method="REML", boxcox=0, constant=10, indicator=pov_gap)
+eb_bhf_reml.fit(y_s, X_s, area_s, intercept=True)
+
+
+def test_eb_bhf_reml():
+    assert eb_bhf_reml.method == "REML"
+
+
+def test_fixed_effects_eb_bhf_reml():
+    assert np.isclose(
+        eb_bhf_reml.fixed_effects, np.array([5.18988986,  2.89804078, -3.00472657,  2.82705747]), atol=1e-1
+    ).all()
+
+
+def test_re_std_eb_bhf_reml():
+    assert np.isclose(eb_bhf_reml.re_std, 0.2500, atol=1e-1)
+
+
+def test_error_var_eb_bhf_reml():
+    assert np.isclose(eb_bhf_reml.error_std, 1.0000, atol=1e-1)
