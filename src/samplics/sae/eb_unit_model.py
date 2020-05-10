d2c7a40caf753177ff94aad93419eadcee9e6502
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-05-10 14:06:06 -0400

    initial commit

--- src/samplics/sae/eb_unit_model.py
+++ src/samplics/sae/eb_unit_model.py
@@ -2,18 +2,38 @@
 
 This module implements the basic EBLUP and EB unit level models. It also provides the ELL model. 
 These functionalities are organized in classes. Each class has three main methods: *fit()*, 
-*predict()* and *bootstrap_mse()*. 
+*predict()* and *bootstrap_mse()*. Linear Mixed Models (LMM) are the core underlying statistical
+framework used to model the hierarchical nature of the small area estimation (SAE) techniques 
+implemented in this module, see McCulloch, C.E. and Searle, S.R. (2001) [#ms2001]_ for more 
+details on LMM.
 
-The EblupUnitLevel class implementes the unit level method initially developed by Battese, G.E., 
-Harter, R.M., and Fuller, W.A. (1988) [#bhf1988]_. The model parameters can fitted using
-restricted maximum likelihood (REML) and maximum likelihood (ML). The normality assumption of the
-errors is not necesary to predict the point estimates but is required for the taylor MSE 
-estimation. The predictions takes into account sampling rates. A bootstrap MSE estimation method 
-is also implemted for this class. 
+The EblupUnitLevel class implementes the model developed by Battese, G.E., Harter, R.M., and 
+Fuller, W.A. (1988) [#bhf1988]_. The model parameters can fitted using restricted maximum 
+likelihood (REML) and maximum likelihood (ML). The normality assumption of the errors is not 
+necesary to predict the point estimates but is required for the taylor MSE estimation. The 
+predictions takes into account sampling rates. A bootstrap MSE estimation method is also implemted 
+for this class. 
+
+The EbUnitLevel class implementes the model developed by Molina, I. and Rao, J.N.K. (2010)
+[#mr2010]_. So far, only the basic approach requiring the normal distribution of the errors is 
+implemented. This approach allows estimating complex indicators such as poverty indices and 
+other nonlinear paramaters. The class fits the model parameters using REML or ML. To predict the 
+area level indicators estimates, a Monte Carlo (MC) approach is used. MSE estimation is achieved 
+using a bootstrap procedure.  
+
+The EllUnitLevel class implements the model Elbers, C., Lanjouw, J.O., and Lanjouw, P. (2003) [#ell2003]_. This method is nonparametric at its core, hence does not require normality 
+assumption nor any other parametric distribution. This implementation a semiparametric and 
+nonparametric are provided. In the semiparametric, the normal distribution is used to fit the parameters and to draw the fixed-effects. 
 
 .. [#bhf1988] Battese, G.E., Harter, R.M., and Fuller, W.A. (1988). An error-components model for 
    prediction of county crop areas using survey and satellite data, *Journal of the American 
    Statistical Association*, **83**, 28-36.
+.. [#mr2010] Molina, , I. and Rao, J.N.K. (2010), Small Area Estimation of Poverty Indicators, 
+   *Canadian Journal of Statistics*, **38**, 369-385.
+.. [#ell2003] Elbers, C., Lanjouw, J.O., and Lanjouw, P. (2003), Micro-Level Estimation of Poverty
+   and Inequality. *Econometrica*, **71**, 355-364.
+.. [#ms2001] McCulloch, C.E.and Searle, S.R. (2001), *Generalized, Linear, Mixed Models*, 
+   New York: John Wiley & Sons, Inc.
 """
 
 from typing import Any, Dict, List, Optional, Tuple, Union, Callable
