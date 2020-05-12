462a424c9db5f3e07bc124916f5a9d194e0d0b81
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-05-12 18:07:56 -0400

    added .travis.yml

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,9 @@ import nox
 
 @nox.session(python=["3.6", "3.7", "3.8"])
 def tests(session):
-    session.install("pytest")
+    session.install("poetry")
+    session.run("poetry", "install")
+    # session.install("pytest")
     session.run("pytest", "-v", "tests")
 
 
