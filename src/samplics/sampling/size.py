bdfcd4d3b0c61a65ba3b628695eaf3bf645a4c34
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-05-31 12:06:59 -0400

    rename class Sample to SampleSelection

---
+++ src/samplics/sampling/size.py
@@ -0,0 +1,20 @@
+"""Sample size calculation module 
+
+"""
+
+from typing import Any, Dict, Tuple, Union, Optional
+
+import numpy as np
+import pandas as pd
+
+import math
+
+from samplics.utils import checks, formats
+from samplics.utils.types import Array, Number, StringNumber
+
+
+class SampleSize:
+    """*SampleSize* implements sample size calculation methods
+    """
+
+    pass
