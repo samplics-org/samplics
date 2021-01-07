ae01cb49066476a876ff200197c1b278a828f4aa
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2021-01-07 13:39:33 -0500

    adding ttests

--- src/samplics/utils/types.py
+++ src/samplics/utils/types.py
@@ -1,7 +1,7 @@
 """Provides the custom types used throughout the modules.
 """
 
-from typing import Dict, Union
+from typing import Dict, TypeVar, Union
 
 import numpy as np
 import pandas as pd
@@ -10,7 +10,7 @@ import pandas as pd
 Array = Union[np.ndarray, pd.Series, list, tuple]
 Series = Union[pd.Series, list, tuple]
 
-Number = Union[float, int]
-StringNumber = Union[str, float, int]
+Number = TypeVar("Number", int, float)  # Union[float, int]
+StringNumber = TypeVar("StringNumber", str, float, int)  # Union[str, float, int]
 
 DictStrNum = Dict[StringNumber, Number]
---
+++ tests/categorical/test_comparison.py
@@ -0,0 +1,9 @@
+
+
+import pandas as pd 
+
+from samplics.categorical.comparison import Ttest
+
+auto = pd.read_csv("./tests/categorical/auto.csv")
+
+print(auto)
