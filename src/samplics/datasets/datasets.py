c7866de697a8278d4a726006cf3ee3d82a6b7a66
Mamadou S Diallo <me@msdiallo.io>
2021-04-22 18:00:13 -0400

    adding datasets module

--- src/samplics/datasets/datasets.py
+++ src/samplics/datasets/datasets.py
@@ -26,8 +26,8 @@ def load_dataset(
     return {
         "name": name,
         "description": description,
-        "ncols": ncols,
         "nrows": nrows,
+        "ncols": ncols,
         "data": df,
         "design": design,
         "source": source,
@@ -51,6 +51,7 @@ def load_psu_frame():
 
 
 def load_psu_sample():
+    colnames = ["cluster", "region", "psu_prob"]
     name = "PSU Sample"
     description = "The PSU sample obtained from the simulated PSU frame."
     design = {}
@@ -58,7 +59,7 @@ def load_psu_sample():
 
     return load_dataset(
         "psu_sample.csv",
-        colnames=None,
+        colnames=colnames,
         name=name,
         description=description,
         design=design,
@@ -67,6 +68,7 @@ def load_psu_sample():
 
 
 def load_ssu_sample():
+    colnames = ["cluster", "household", "ssu_prob"]
     name = "SSU Sample"
     description = "The SSU sample obtained from the simulated SSU frame."
     design = {}
@@ -74,7 +76,7 @@ def load_ssu_sample():
 
     return load_dataset(
         "ssu_sample.csv",
-        colnames=None,
+        colnames=colnames,
         name=name,
         description=description,
         design=design,
@@ -161,8 +163,8 @@ def load_nmhis():
 
 def load_auto():
     colnames = None
-    name = "Birth Sample"
-    description = "The Birth sample data."
+    name = "Auto Sample"
+    description = "The Auto sample data."
     design = {}
     source = ""
 
@@ -178,8 +180,8 @@ def load_auto():
 
 def load_birth():
     colnames = None
-    name = "Auto Sample"
-    description = "The Auto sample data."
+    name = "Birth Sample"
+    description = "The Birth sample data."
     design = {}
     source = ""
 
@@ -194,7 +196,7 @@ def load_birth():
 
 
 def load_county_crop():
-    colnames = None
+    colnames = ["county_id", "corn_area", "soybeans_area", "corn_pixel", "soybeans_pixel"]
     name = "County Crop Sample"
     description = "The County Crop Areas sample data."
     design = {}
@@ -211,7 +213,13 @@ def load_county_crop():
 
 
 def load_county_crop_means():
-    colnames = None
+    colnames = [
+        "county_id",
+        "samp_segments",
+        "pop_segments",
+        "ave_corn_pixel",
+        "ave_soybeans_pixel",
+    ]
     name = "County Crop Area Means"
     description = "The County Crop Area Means data."
     design = {}
@@ -228,7 +236,14 @@ def load_county_crop_means():
 
 
 def load_expenditure_milk():
-    colnames = None
+    colnames = [
+        "major_area",
+        "small_area",
+        "samp_size",
+        "direct_est",
+        "std_error",
+        "coef_var",
+    ]
     name = "Expenditure on Milk"
     description = "The expenditure on milk data."
     design = {}
@@ -242,299 +257,3 @@ def load_expenditure_milk():
         design=design,
         source=source,
     )
-
-
-class _Dataset:
-    """The base class for the datasets included on the library"""
-
-    def __init__(self) -> None:
-        self.name: str = ""
-        self.description: str = ""
-        self.ncols: int = 0
-        self.nrows: int = 0
-        self.design: dict = {}
-        self.source: dict = {}
-        self.data = pd.DataFrame
-
-    def _load_data(self, file_name: str, colnames: Optional[list] = None) -> None:
-
-        module_path = dirname(__file__)
-        file_path = join(module_path, "data", file_name)
-        df = pd.read_csv(file_path)
-        if colnames is not None:
-            df = df[colnames]
-        self.nrows, self.ncols = df.shape
-
-        self.data = df
-
-
-class PSUFrame(_Dataset):
-    """A class to represent the PSU frame dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "PSU Frame"
-        self.description = "A simulated census data."
-
-    def load_data(self) -> None:
-        """Load psu_frame.csv file to create the data member"""
-
-        self._load_data("psu_frame.csv")
-
-
-class PSUSample(_Dataset):
-    """A class to represent the PSU sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "PSU Sample"
-        self.description = "The PSU sample obtained from the simulated PSU frame."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load psu_sample.csv file to create the data member"""
-
-        self._load_data("psu_sample.csv", colnames=["cluster", "region", "psu_prob"])
-
-
-class SSUSample(_Dataset):
-    """A class to represent the SSU sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "SSU Sample"
-        self.description = "The SSU sample obtained from the simulated SSU frame."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load ssu_sample.csv file to create the data member"""
-
-        self._load_data("ssu_sample.csv", colnames=["cluster", "household", "ssu_prob"])
-
-
-class Nhanes2(_Dataset):
-    """A class to represent a subset of the NHANES II sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "NHANES II Subsample"
-        self.description = "A subset of NHANES II data. This file is not meant to be representative of NHANES II. It is just an subset to illustrate the syntax in this tutorial."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load nhanes2.csv file to create the data member"""
-
-        self._load_data(
-            "nhanes2.csv",
-            colnames=[
-                "stratid",
-                "psuid",
-                "race",
-                "highbp",
-                "highlead",
-                "zinc",
-                "diabetes",
-                "finalwgt",
-            ],
-        )
-
-
-class Nhanes2brr(_Dataset):
-    """A class to represent a subset of the NHANES II sample dataset with bootstrap weights
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "NHANES II Subsample with bootstrap weights"
-        self.description = "A subset of NHANES II data with bootstrap weights. This file is not meant to be representative of NHANES II. It is just an subset to illustrate the syntax in this tutorial."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load nhanes2brr_subset.csv file to create the data member"""
-
-        self._load_data("nhanes2brr_subset.csv")
-
-
-class Nhanes2jk(_Dataset):
-    """A class to represent a subset of the NHANES II sample dataset with jackknife weights
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "NHANES II Subsample with jackknife weights"
-        self.description = "A subset of NHANES II data with jackknife weights. This file is not meant to be representative of NHANES II. It is just an subset to illustrate the syntax in this tutorial."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load nhanes2jk_subset.csv file to create the data member"""
-
-        self._load_data("nhanes2jk_subset.csv")
-
-
-class Nmihs(_Dataset):
-    """A class to represent the NMIHS sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "NMIHS Subsample"
-        self.description = "A subset of NMHIS data. This file is not meant to be representative of NMHIS. It is just an subset to illustrate the syntax in this tutorial."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load nmihs_subset.csv file to create the data member"""
-
-        self._load_data("nmihs_subset.csv")
-
-
-class Auto(_Dataset):
-    """A class to represent the Auto sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "Auto Sample"
-        self.description = "The Auto sample data."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load auto.csv file to create the data member"""
-
-        self._load_data("auto.csv", colnames=["mpg", "foreign", "y1", "y2"])
-
-
-class Birth(_Dataset):
-    """A class to represent the Birth sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "Birth Sample"
-        self.description = "The Birth sample data."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load birth.csv file to create the data member"""
-
-        self._load_data("birth.csv", colnames=["region", "agecat", "birthcat", "pop"])
-
-
-class CountyCrop(_Dataset):
-    """A class to represent the County Crop Areas sample dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "County Crop Areas Sample"
-        self.description = "The County Crop Areas sample data."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load countycrop.csv file to create the data member"""
-
-        self._load_data(
-            "countycrop.csv",
-            colnames=["county_id", "corn_area", "soybeans_area", "corn_pixel", "soybeans_pixel"],
-        )
-
-
-class CountyCropMeans(_Dataset):
-    """A class to represent the County Crop Area Means dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "County Crop Area Means"
-        self.description = "The County Crop Area Means data."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load countycrop_means.csv file to create the data member"""
-
-        self._load_data(
-            "countycrop_means.csv",
-            colnames=[
-                "county_id",
-                "samp_segments",
-                "pop_segments",
-                "ave_corn_pixel",
-                "ave_soybeans_pixel",
-            ],
-        )
-
-
-class ExpenditureMilk(_Dataset):
-    """A class to represent the expenditure on milk dataset
-
-    Args:
-        _Dataset ([type]): The base class for loading datasets
-    """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "Expenditure on Milk"
-        self.description = "The expenditure on milk data."
-
-    def load_data(
-        self,
-    ) -> None:
-        """Load expenditure_on_milk.csv file to create the data member"""
-
-        self._load_data(
-            "expenditure_on_milk.csv",
-            colnames=[
-                "major_area",
-                "small_area",
-                "samp_size",
-                "direct_est",
-                "std_error",
-                "coef_var",
-            ],
-        )
--- tests/datasets/test_datasets.py
+++ tests/datasets/test_datasets.py
@@ -1,28 +1,27 @@
 from samplics.datasets.datasets import (
-    Auto,
-    Birth,
-    CountyCrop,
-    CountyCropMeans,
-    ExpenditureMilk,
-    Nhanes2,
-    Nhanes2brr,
-    Nhanes2jk,
-    Nmihs,
-    PSUFrame,
-    PSUSample,
-    SSUSample,
+    load_auto,
+    load_birth,
+    load_county_crop,
+    load_county_crop_means,
+    load_expenditure_milk,
+    load_nhanes2,
+    load_nhanes2brr,
+    load_nhanes2jk,
+    load_nmhis,
+    load_psu_frame,
+    load_psu_sample,
+    load_ssu_sample,
 )
 
 
 def test_loading_psu_frame():
-    psu_frame = PSUFrame()
-    psu_frame.load_data()
+    psu_frame = load_psu_frame()
 
-    assert psu_frame.name == "PSU Frame"
-    assert psu_frame.description == "A simulated census data."
-    assert psu_frame.nrows == 100
-    assert psu_frame.ncols == 5
-    assert list(psu_frame.data.columns) == [
+    assert psu_frame["name"] == "PSU Frame"
+    assert psu_frame["description"] == "A simulated census data."
+    assert psu_frame["nrows"] == 100
+    assert psu_frame["ncols"] == 5
+    assert list(psu_frame["data"].columns) == [
         "cluster",
         "region",
         "number_households_census",
@@ -33,39 +32,37 @@ def test_loading_psu_frame():
 
 # breakpoint()
 def test_loading_psu_sample():
-    psu_sample = PSUSample()
-    psu_sample.load_data()
+    psu_sample = load_psu_sample()
 
-    assert psu_sample.name == "PSU Sample"
-    assert psu_sample.description == "The PSU sample obtained from the simulated PSU frame."
-    assert psu_sample.nrows == 10
-    assert psu_sample.ncols == 3
-    assert list(psu_sample.data.columns) == ["cluster", "region", "psu_prob"]
+    assert psu_sample["name"] == "PSU Sample"
+    assert psu_sample["description"] == "The PSU sample obtained from the simulated PSU frame."
+    assert psu_sample["nrows"] == 10
+    assert psu_sample["ncols"] == 3
+    assert list(psu_sample["data"].columns) == ["cluster", "region", "psu_prob"]
 
 
 def test_loading_ssu_sample():
-    ssu_sample = SSUSample()
-    ssu_sample.load_data()
 
-    assert ssu_sample.name == "SSU Sample"
-    assert ssu_sample.description == "The SSU sample obtained from the simulated SSU frame."
-    assert ssu_sample.nrows == 150
-    assert ssu_sample.ncols == 3
-    assert list(ssu_sample.data.columns) == ["cluster", "household", "ssu_prob"]
+    ssu_sample = load_ssu_sample()
+
+    assert ssu_sample["name"] == "SSU Sample"
+    assert ssu_sample["description"] == "The SSU sample obtained from the simulated SSU frame."
+    assert ssu_sample["nrows"] == 150
+    assert ssu_sample["ncols"] == 3
+    assert list(ssu_sample["data"].columns) == ["cluster", "household", "ssu_prob"]
 
 
 def test_loading_nhanes2_subset():
-    nhanes2 = Nhanes2()
-    nhanes2.load_data()
+    nhanes2 = load_nhanes2()
 
-    assert nhanes2.name == "NHANES II Subsample"
+    assert nhanes2["name"] == "NHANES II Subsample"
     assert (
-        nhanes2.description
+        nhanes2["description"]
         == "A subset of NHANES II data. This file is not meant to be representative of NHANES II. It is just an subset to illustrate the syntax in this tutorial."
     )
-    assert nhanes2.nrows == 10337
-    assert nhanes2.ncols == 8
-    assert list(nhanes2.data.columns) == [
+    assert nhanes2["nrows"] == 10337
+    assert nhanes2["ncols"] == 8
+    assert list(nhanes2["data"].columns) == [
         "stratid",
         "psuid",
         "race",
@@ -78,75 +75,69 @@ def test_loading_nhanes2_subset():
 
 
 def test_loading_nnhanes2brr_subset():
-    nhanes2brr_subset = Nhanes2brr()
-    nhanes2brr_subset.load_data()
+    nhanes2brr_subset = load_nhanes2brr()
 
-    assert nhanes2brr_subset.name == "NHANES II Subsample with bootstrap weights"
+    assert nhanes2brr_subset["name"] == "NHANES II Subsample with bootstrap weights"
     assert (
-        nhanes2brr_subset.description
+        nhanes2brr_subset["description"]
         == "A subset of NHANES II data with bootstrap weights. This file is not meant to be representative of NHANES II. It is just an subset to illustrate the syntax in this tutorial."
     )
-    assert nhanes2brr_subset.nrows == 1347
-    assert nhanes2brr_subset.ncols == 35
+    assert nhanes2brr_subset["nrows"] == 1347
+    assert nhanes2brr_subset["ncols"] == 35
 
 
 def test_loading_nnhanes2jk_subset():
-    nhanes2jk_subset = Nhanes2jk()
-    nhanes2jk_subset.load_data()
+    nhanes2jk_subset = load_nhanes2jk()
 
-    assert nhanes2jk_subset.name == "NHANES II Subsample with jackknife weights"
+    assert nhanes2jk_subset["name"] == "NHANES II Subsample with jackknife weights"
     assert (
-        nhanes2jk_subset.description
+        nhanes2jk_subset["description"]
         == "A subset of NHANES II data with jackknife weights. This file is not meant to be representative of NHANES II. It is just an subset to illustrate the syntax in this tutorial."
     )
-    assert nhanes2jk_subset.nrows == 887
-    assert nhanes2jk_subset.ncols == 65
+    assert nhanes2jk_subset["nrows"] == 887
+    assert nhanes2jk_subset["ncols"] == 65
 
 
 def test_loading_nmihs_subset():
-    nmihs_subset = Nmihs()
-    nmihs_subset.load_data()
+    nmihs_subset = load_nmhis()
 
-    assert nmihs_subset.name == "NMIHS Subsample"
+    assert nmihs_subset["name"] == "NMIHS Subsample"
     assert (
-        nmihs_subset.description
+        nmihs_subset["description"]
         == "A subset of NMHIS data. This file is not meant to be representative of NMHIS. It is just an subset to illustrate the syntax in this tutorial."
     )
-    assert nmihs_subset.nrows == 603
-    assert nmihs_subset.ncols == 52
+    assert nmihs_subset["nrows"] == 603
+    assert nmihs_subset["ncols"] == 52
 
 
 def test_loading_auto():
-    auto = Auto()
-    auto.load_data()
+    auto = load_auto()
 
-    assert auto.name == "Auto Sample"
-    assert auto.description == "The Auto sample data."
-    assert auto.nrows == 74
-    assert auto.ncols == 4
-    assert list(auto.data.columns) == ["mpg", "foreign", "y1", "y2"]
+    assert auto["name"] == "Auto Sample"
+    assert auto["description"] == "The Auto sample data."
+    assert auto["nrows"] == 74
+    assert auto["ncols"] == 4
+    assert list(auto["data"].columns) == ["mpg", "foreign", "y1", "y2"]
 
 
 def test_loading_birth():
-    birth = Birth()
-    birth.load_data()
+    birth = load_birth()
 
-    assert birth.name == "Birth Sample"
-    assert birth.description == "The Birth sample data."
-    assert birth.nrows == 956
-    assert birth.ncols == 4
-    assert list(birth.data.columns) == ["region", "agecat", "birthcat", "pop"]
+    assert birth["name"] == "Birth Sample"
+    assert birth["description"] == "The Birth sample data."
+    assert birth["nrows"] == 956
+    assert birth["ncols"] == 4
+    assert list(birth["data"].columns) == ["region", "agecat", "birthcat", "pop"]
 
 
 def test_loading_county_crop_areas():
-    county_crop = CountyCrop()
-    county_crop.load_data()
+    county_crop = load_county_crop()
 
-    assert county_crop.name == "County Crop Areas Sample"
-    assert county_crop.description == "The County Crop Areas sample data."
-    assert county_crop.nrows == 37
-    assert county_crop.ncols == 5
-    assert list(county_crop.data.columns) == [
+    assert county_crop["name"] == "County Crop Sample"
+    assert county_crop["description"] == "The County Crop Areas sample data."
+    assert county_crop["nrows"] == 37
+    assert county_crop["ncols"] == 5
+    assert list(county_crop["data"].columns) == [
         "county_id",
         "corn_area",
         "soybeans_area",
@@ -156,14 +147,13 @@ def test_loading_county_crop_areas():
 
 
 def test_loading_county_crop_areas_means():
-    county_crop_means = CountyCropMeans()
-    county_crop_means.load_data()
+    county_crop_means = load_county_crop_means()
 
-    assert county_crop_means.name == "County Crop Area Means"
-    assert county_crop_means.description == "The County Crop Area Means data."
-    assert county_crop_means.nrows == 12
-    assert county_crop_means.ncols == 5
-    assert list(county_crop_means.data.columns) == [
+    assert county_crop_means["name"] == "County Crop Area Means"
+    assert county_crop_means["description"] == "The County Crop Area Means data."
+    assert county_crop_means["nrows"] == 12
+    assert county_crop_means["ncols"] == 5
+    assert list(county_crop_means["data"].columns) == [
         "county_id",
         "samp_segments",
         "pop_segments",
@@ -173,14 +163,13 @@ def test_loading_county_crop_areas_means():
 
 
 def test_loading_expenditure_on_milk():
-    expenditure_milk = ExpenditureMilk()
-    expenditure_milk.load_data()
+    expenditure_milk = load_expenditure_milk()
 
-    assert expenditure_milk.name == "Expenditure on Milk"
-    assert expenditure_milk.description == "The expenditure on milk data."
-    assert expenditure_milk.nrows == 43
-    assert expenditure_milk.ncols == 6
-    assert list(expenditure_milk.data.columns) == [
+    assert expenditure_milk["name"] == "Expenditure on Milk"
+    assert expenditure_milk["description"] == "The expenditure on milk data."
+    assert expenditure_milk["nrows"] == 43
+    assert expenditure_milk["ncols"] == 6
+    assert list(expenditure_milk["data"].columns) == [
         "major_area",
         "small_area",
         "samp_size",
