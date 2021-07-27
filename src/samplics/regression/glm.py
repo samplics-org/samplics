8aece40bf00d73ed756a3b08e00eea046e9936c5
Mamadou S Diallo <me@msdiallo.io>
2021-07-27 19:02:52 -0400

    adding survey glm

--- src/samplics/regression/glm.py
+++ src/samplics/regression/glm.py
@@ -16,18 +16,24 @@ class SurveyGLM:
     """General linear models under complex survey sampling"""
 
     def __init__(self):
-        pass
+        self.beta: np.ndarray
 
     @staticmethod
     def _residuals(e: np.ndarray, psu: np.ndarray, nb_vars: Number) -> tuple(np.ndarray, Number):
 
-        psus, nh = np.unique(psu, return_counts=True)
-        e_values = np.zeros(psus.shape[0], nb_vars)
-        for i, p in enumerate(np.unique(psus.shape(0))):
-            e_values[i, :] += np.sum(e[psu == p, :], axis=0)
-        e_means = np.sum(e_values, axis=0) / nh
+        psus = np.unique(psu)
+        if psus.shape[0] == 1 and e.shape[0] == 1:
+            raise AssertionError("Only one observation in the stratum")
+        if psus.shape[0] == 1:
+            psu = np.arange(e.shape[0])
+            psus = np.unique(psu)
+        e_values = np.zeros((psus.shape[0], nb_vars))
 
-        return np.transpose(e_values - e_means) @ (e_values - e_means), nh
+        for i, p in enumerate(np.unique(psus)):
+            e_values[i, :] += np.sum(e[psu == p, :], axis=0)
+        e_means = np.sum(e_values, axis=0) / psus.shape[0]
+
+        return np.transpose(e_values - e_means) @ (e_values - e_means), psus.shape[0]
 
     def _calculate_g(
         self,
@@ -36,23 +42,26 @@ class SurveyGLM:
         x: np.ndarray,
         stratum: Optional[np.ndarray],
         psu: Optional[np.ndarray],
+        fpc: Union[dict[StringNumber, Number], Number],
+        glm_scale=Number,
     ) -> np.ndarray:
 
-        e = samp_weight * resid * x
-
+        e = (samp_weight * resid)[:, None] * x / glm_scale
+        if psu is None:
+            psu = np.arange(e.shape[0])
         if stratum is None:
             e_h, n_h = self._residuals(e=e, psu=psu, nb_vars=x.shape[1])
-            return (n_h * (1 - self.fpc) / (n_h - 1)) * e_h
+            return fpc * (n_h / (n_h - 1)) * e_h
         else:
-            g_h = np.zeros(x.shape)
+            g_h = np.zeros((x.shape[1], x.shape[1]))
             for s in np.unique(stratum):
                 e_s = e[stratum == s, :]
                 psu_s = psu[stratum == s]
                 e_h, n_h = self._residuals(e=e_s, psu=psu_s, nb_vars=x.shape[1])
-                g_h += (n_h * (1 - self.fpc[s]) / (n_h - 1)) * e_h
+                g_h += fpc[s] * (n_h / (n_h - 1)) * e_h
             return g_h
 
-    def fit(
+    def estimate(
         self,
         y: Array,
         x: Optional[Array] = None,
@@ -63,6 +72,21 @@ class SurveyGLM:
         remove_nan: bool = False,
     ) -> None:
 
+        y = numpy_array(y)
+        y_temp = y.copy()
+
+        x = numpy_array(x) if x is not None else None
+        psu = numpy_array(psu) if psu is not None else None
+
+        if samp_weight is None:
+            weight_temp = np.ones(y.shape[0])
+        elif isinstance(samp_weight, (float, int)):
+            weight_temp = samp_weight * np.ones(y_temp.shape[0])
+        elif isinstance(samp_weight, np.ndarray):
+            weight_temp = samp_weight.copy()
+        else:
+            weight_temp = np.asarray(samp_weight)
+
         if not isinstance(fpc, dict):
             self.fpc = fpc_as_dict(stratum, fpc)
         else:
@@ -71,14 +95,20 @@ class SurveyGLM:
             else:
                 self.fpc = fpc
 
-        glm_model = sm.GLM(y=y, x=x, var_weights=samp_weight)
+        glm_model = sm.GLM(endog=y_temp, exog=x, var_weights=weight_temp)
         glm_results = glm_model.fit()
 
         g = self._calculate_g(
-            samp_weight=samp_weight, resid=glm_results.resid_response, x=x, psu=psu, fpc=fpc
+            samp_weight=samp_weight,
+            resid=glm_results.resid_response,
+            x=x,
+            stratum=stratum,
+            psu=psu,
+            fpc=self.fpc,
+            glm_scale=glm_results.scale,
         )
 
-        d = glm_results.cov_params() / glm_results.scale
+        d = glm_results.cov_params()
 
         self.beta = glm_results.params
-        self.cov_beta = d * g * d
+        self.cov_beta = (d @ g) @ d
--- tests/regression/test_glm.py
+++ tests/regression/test_glm.py
@@ -9,14 +9,36 @@ api_strat = pd.read_csv("./tests/regression/api_strat.csv")
 y = api_strat["api00"]
 x = api_strat[["ell", "meals", "mobility"]]
 x.insert(0, "intercept", 1)
-
-# breakpoint()
+stratum = api_strat["stype"]
+psu = api_strat["dnum"]
 weight = api_strat["pw"]
 
-svyglm = SurveyGLM()
-svyglm_results = svyglm.fit(y=y, x=x, samp_weight=weight)
 
-svyglm_results.summary()
+def test_survey_glm_not_stratified_srs():
+    svyglm = SurveyGLM()
+    svyglm.estimate(y=y, x=x, samp_weight=weight)
 
-# breakpoint()
+    assert np.isclose(svyglm.beta[0], 820.8873)
+    assert np.isclose(svyglm.beta[1], -0.4805866)
+    assert np.isclose(svyglm.beta[2], -3.141535)
+    assert np.isclose(svyglm.beta[3], 0.2257132)
 
+    assert np.isclose(svyglm.cov_beta[0, 0], 10.97091 ** 2)
+    assert np.isclose(svyglm.cov_beta[1, 1], 0.3971755 ** 2)
+    assert np.isclose(svyglm.cov_beta[2, 2], 0.2917333 ** 2)
+    assert np.isclose(svyglm.cov_beta[3, 3], 0.4012498 ** 2)
+
+
+def test_survey_glm_stratified_srs():
+    svyglm = SurveyGLM()
+    svyglm.estimate(y=y, x=x, samp_weight=weight, stratum=stratum)
+
+    assert np.isclose(svyglm.beta[0], 820.8873)
+    assert np.isclose(svyglm.beta[1], -0.4805866)
+    assert np.isclose(svyglm.beta[2], -3.141535)
+    assert np.isclose(svyglm.beta[3], 0.2257132)
+
+    assert np.isclose(svyglm.cov_beta[0, 0], 10.25649 ** 2)
+    assert np.isclose(svyglm.cov_beta[1, 1], 0.3977075 ** 2)
+    assert np.isclose(svyglm.cov_beta[2, 2], 0.2883001 ** 2)
+    assert np.isclose(svyglm.cov_beta[3, 3], 0.4026908 ** 2)
