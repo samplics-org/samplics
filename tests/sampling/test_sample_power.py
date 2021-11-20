0a2053b7016418903c33216bbb8adc8c64282dd4
Mamadou S Diallo <me@msdiallo.io>
2021-11-20 14:54:41 -0500

    adding functions for power calculation

--- tests/sampling/test_sample_power.py
+++ tests/sampling/test_sample_power.py
@@ -67,7 +67,7 @@ def test_power_for_one_prop_two_sides_dict():
     assert np.isclose(power["four"], 0.3200, atol=0.001)
 
 
-def test_power_for_one_prop_one_sides_number():
+def test_power_for_one_prop_one_less_number():
 
     samp_size = np.array([75, 55, 37, 5])
     prop_0 = (0.2, 0.37, 0.95, 0.85)
@@ -87,7 +87,7 @@ def test_power_for_one_prop_one_sides_number():
     assert np.isclose(power_less[3], 0.0236, atol=0.001)
 
 
-def test_power_for_one_prop_one_sides_number():
+def test_power_for_one_prop_greater_number():
 
     samp_size = {1: 75, 2: 55, 3: 37, 4: 5}
     prop_0 = {1: 0.2, 2: 0.37, 3: 0.95, 4: 0.85}
@@ -105,3 +105,87 @@ def test_power_for_one_prop_one_sides_number():
     assert np.isclose(power_less[2], 0.4446, atol=0.001)
     assert np.isclose(power_less[3], 0.4530, atol=0.001)
     assert np.isclose(power_less[4], 0.0960, atol=0.001)
+
+
+def test_power_for_one_prop_two_sides_number_not_arcsin():
+    power1 = power_for_one_proportion(
+        samp_size=107, prop_0=0.2, prop_1=0.1, arcsin=False, testing_type="two-side", alpha=0.05
+    )
+    power2 = power_for_one_proportion(
+        samp_size=25, prop_0=0.3, prop_1=0.7, arcsin=False, testing_type="two-side", alpha=0.10
+    )
+    power3 = power_for_one_proportion(
+        samp_size=500, prop_0=0.95, prop_1=0.99, arcsin=False, testing_type="two-side", alpha=0.07
+    )
+    power4 = power_for_one_proportion(
+        samp_size=50,
+        prop_0=0.0950,
+        prop_1=0.1990,
+        arcsin=False,
+        testing_type="two-side",
+        alpha=0.01,
+    )
+    assert np.isclose(power1, 0.9316, atol=0.001)
+    assert np.isclose(power2, 0.9967, atol=0.001)
+    assert np.isclose(power3, 0.9999, atol=0.001)
+    assert np.isclose(power4, 0.2315, atol=0.001)
+
+
+def test_power_for_one_prop_two_sides_array_not_arcsin():
+    samp_size = np.array([107, 25, 500, 50])
+    prop_0 = (0.2, 0.3, 0.95, 0.0950)
+    prop_1 = [0.1, 0.7, 0.99, 0.1990]
+    alpha = [0.05, 0.10, 0.07, 0.01]
+    power = power_for_one_proportion(
+        samp_size=samp_size,
+        prop_0=prop_0,
+        prop_1=prop_1,
+        arcsin=False,
+        testing_type="two-side",
+        alpha=alpha,
+    )
+
+    assert np.isclose(power[0], 0.9316, atol=0.001)
+    assert np.isclose(power[1], 0.9967, atol=0.001)
+    assert np.isclose(power[2], 0.9999, atol=0.001)
+    assert np.isclose(power[3], 0.2315, atol=0.001)
+
+
+def test_power_for_one_prop_two_sides_dict_not_arcsin():
+    samp_size = {"one": 107, "two": 25, "three": 500, "four": 50}
+    prop_0 = {"one": 0.2, "two": 0.3, "three": 0.95, "four": 0.095}
+    prop_1 = {"one": 0.1, "two": 0.7, "three": 0.99, "four": 0.199}
+    alpha = {"one": 0.05, "two": 0.10, "three": 0.07, "four": 0.01}
+    power = power_for_one_proportion(
+        samp_size=samp_size,
+        prop_0=prop_0,
+        prop_1=prop_1,
+        arcsin=False,
+        testing_type="two-side",
+        alpha=alpha,
+    )
+
+    assert np.isclose(power["one"], 0.9316, atol=0.001)
+    assert np.isclose(power["two"], 0.9967, atol=0.001)
+    assert np.isclose(power["three"], 0.9999, atol=0.001)
+    assert np.isclose(power["four"], 0.2315, atol=0.001)
+
+
+def test_power_for_one_prop_one_less_number_not_arcsin():
+
+    samp_size = np.array([75, 55, 37, 5])
+    prop_0 = (0.2, 0.37, 0.95, 0.85)
+    prop_1 = [0.1, 0.47, 0.99, 0.90]
+    power_less = power_for_one_proportion(
+        samp_size=samp_size,
+        prop_0=prop_0,
+        prop_1=prop_1,
+        arcsin=False,
+        testing_type="less",
+        alpha=0.05,
+    )
+
+    assert np.isclose(power_less[0], 0.8929, atol=0.001)
+    assert np.isclose(power_less[1], 0.0008, atol=0.001)
+    assert np.isclose(power_less[2], 0.0008, atol=0.001)
+    assert np.isclose(power_less[3], 0.0218, atol=0.001)
